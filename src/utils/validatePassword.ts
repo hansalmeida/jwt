52bec7e15c02380f855653803f1c5d22b5303ebc
hansalmeida <hansalmeida@hotmail.com>
2022-01-28 07:10:37 -0300

    Remove console.log

--- src/utils/validatePassword.ts
+++ src/utils/validatePassword.ts
@@ -1,7 +1,6 @@
 import { compare } from "bcryptjs"
 
 export const validatePassword = async (password: string, hash: string) => {
-  console.log(hash, password)
   const isValid = await compare(hash, password)
 
   return isValid
