47ee2a1d3bf977690e6dc6c48845a0c8397c32d8
hansalmeida <hansalmeida@hotmail.com>
2022-01-28 04:51:26 -0300

    Configure json + cors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
 const port = 3333
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" })
+})
 
 app.listen(port, () => {
   console.log(`App listening on localhost:${port}`)
